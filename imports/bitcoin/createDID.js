853dd1b6b2392de0c931e113e2c13747209db303
Federico Squartini <federico.squartini@gmail.com>
2017-06-30 11:18:02 +0200

    Complete DID creation function and tests

--- imports/bitcoin/createDID.js
+++ imports/bitcoin/createDID.js
@@ -1,10 +1,70 @@
+import coinSelect from '/imports/bitcoin/coinSelect'
+import {Meteor} from 'meteor/meteor'
+import _ from 'underscore'
+
 global.Buffer = global.Buffer || require('buffer').Buffer
-const bip39 = require('bip39')
 const bitcoin = require('bitcoinjs-lib')
 
-export default (identityAccount, walletRoot) => {
-  //const seed = bip39.mnemonicToSeed(mnemonic)
-  //const root = bitcoin.HDNode.fromSeedBuffer(seed)
-  //return root.toBase58()
-  return null
+export default async ({
+  identityAccount,
+  walletRoot,
+  fundingKeypair,
+  amount,
+  recoveryAddress,
+  utxos
+}) => {
+  const identityRoot = walletRoot.derivePath("m/44'/0'")
+    .deriveHardened(identityAccount)
+    .derive(0)
+  const changeKeypair = fundingKeypair
+  const feeRate = Meteor.settings.public.feeRate
+  const signatureKeyPair = identityRoot.derive(0)
+  const signatureAddress = signatureKeyPair.getAddress()
+  const network = bitcoin.networks[Meteor.settings.public.network]
+  /*
+  let utxos
+  try {
+    utxos = await Meteor.callPromise('blockchaininfo.listUtxos', fundingKeypair.getAddress())
+  } catch (e) {
+    // error popup
+    console.error(e)
+    return
+  }
+  */
+  const targets = [
+    {
+      address: signatureAddress,
+      value: amount
+    },
+    {
+      address: recoveryAddress,
+      value: amount
+    }
+  ]
+  const {inputs, outputs} = coinSelect(utxos, targets, feeRate)
+  if (!inputs || !outputs) {
+    throw new Meteor.Error('createDID', 'No funds in the address')
+  }
+  const didTx = new bitcoin.TransactionBuilder(network)
+
+    /*
+     //output of blockchain.info
+  _.each(inputs, input => {
+    didTx.addInput(input.tx_hash, input.tx_output_n)
+  })
+  */
+  // output of listUnspent
+  _.each(inputs, input => {
+    didTx.addInput(input.txid, input.vout)
+  })
+  _.each(outputs, output => {
+    if (!output.address) {
+      output.address = changeKeypair.getAddress()
+    }
+    didTx.addOutput(output.address, output.value)
+  })
+  _.each(inputs, (input, i) => {
+    didTx.sign(i, fundingKeypair)
+  })
+  return didTx.build()
 }
--- imports/bitcoin/createDID.tests.js
+++ imports/bitcoin/createDID.tests.js
@@ -3,24 +3,59 @@
 import {assert} from 'chai'
 import {Meteor} from 'meteor/meteor'
 
-import createHDWallet from './createHDWallet'
 import createDID from './createDID'
 
 global.Buffer = global.Buffer || require('buffer').Buffer
 const bip39 = require('bip39')
 const bitcoin = require('bitcoinjs-lib')
 
+const network = bitcoin.networks.testnet
+
+const createTestKeypair = async () => {
+  const address = await Meteor.callPromise('bitcoin', 'getNewAddress')
+  await Meteor.callPromise('bitcoin', 'sendToAddress', address, 1)
+  const privateKey = await Meteor.callPromise('bitcoin', 'dumpPrivKey', address)
+  // this can be omitted because I take utxos with zero confirms
+  // await Meteor.callPromise('bitcoin', 'generate', 10)
+  return bitcoin.ECPair.fromWIF(privateKey, network)
+}
+
 if (Meteor.isClient) {
   describe('createDID', function () {
-    it('it should create a new DID', function () {
-      const identityAccount = 1
+    it('it should create a new DID', async function (done) {
       const mnemonic = 'praise you muffin lion enable neck grocery crumble super myself license ghost'
       const seed = bip39.mnemonicToSeed(mnemonic)
-      const root = bitcoin.HDNode.fromSeedBuffer(seed)
-      const did = createDID(identityAccount, root)
+      const walletRoot = bitcoin.HDNode.fromSeedBuffer(seed, network)
+      let fundingKeypair
+      try {
+        fundingKeypair = await createTestKeypair()
+      } catch (e) {
+        console.error(e)
+      }
+      const fundingAddress = fundingKeypair.getAddress()
+      const recoveryAddress = 'mgkbNPFpgkgPFLibMzVCPq3f64WB3WhExN'
+      const amount = 1000
+      const utxos = await Meteor.callPromise('bitcoin', 'listUnspent', 0, 9999999,
+        [fundingAddress])
+      const did = await createDID({
+        identityAccount: 1,
+        walletRoot,
+        fundingKeypair,
+        amount,
+        recoveryAddress,
+        utxos})
       assert.isObject(did, 'DID is an object')
       assert.instanceOf(did, bitcoin.Transaction, 'DID is instance of Transaction')
-      assert.equal(did.outs.length, 2, 'DID has two outputs')
+      assert.equal(did.outs.length, 3, 'DID has two outputs')
+      const [{script: sigScript}, {script: recScript}] = did.outs
+      const sigAddress = bitcoin.address.fromOutputScript(sigScript, network)
+      const identityRoot = walletRoot.derivePath("m/44'/0'/1'/0")
+      const signatureKeyPair = identityRoot.derive(0)
+      const signatureAddress = signatureKeyPair.getAddress()
+      assert.equal(sigAddress, signatureAddress)
+      const recAddress = bitcoin.address.fromOutputScript(recScript, network)
+      assert.equal(recAddress, recoveryAddress)
+      done()
     })
   })
 }
