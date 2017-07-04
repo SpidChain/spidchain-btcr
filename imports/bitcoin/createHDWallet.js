da87b530eeb08c740a7da4cb9a8800e5e95e9231
Nicola Squartini <tensor5@gmail.com>
2017-07-04 17:21:23 +0200

    Support testnet during wallet creation

--- imports/bitcoin/createHDWallet.js
+++ imports/bitcoin/createHDWallet.js
@@ -1,9 +1,12 @@
+import {Meteor} from 'meteor/meteor'
+
 global.Buffer = global.Buffer || require('buffer').Buffer
 const bip39 = require('bip39')
-const bitcoin = require('bitcoinjs-lib')
+const {HDNode, networks} = require('bitcoinjs-lib')
+const network = networks[Meteor.settings.public.network]
 
 export default (mnemonic) => {
   const seed = bip39.mnemonicToSeed(mnemonic)
-  const root = bitcoin.HDNode.fromSeedBuffer(seed)
+  const root = HDNode.fromSeedBuffer(seed, network)
   return root.toBase58()
 }
