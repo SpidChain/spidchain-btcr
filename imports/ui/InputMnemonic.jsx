dd7141e559a3bce5058edd5b42ba107cb3675667
Federico Squartini <federico.squartini@gmail.com>
2017-07-13 16:25:44 +0200

    Improve the look of back and confirm buttons, start numbering words inputs from 1

--- imports/ui/InputMnemonic.jsx
+++ imports/ui/InputMnemonic.jsx
@@ -13,16 +13,16 @@ const wordsHandler = (onWords) => (e) => {
 const Row = ({num}) => (
   <FormGroup row>
     <Col xs='3'>
-      <Input type='text' autoCapitalize='none' name={'w' + (4 * num)} placeholder={(4 * num)} />
+      <Input type='text' autoCapitalize='none' name={'w' + (4 * num)} placeholder={4 * num + 1} />
     </Col>
     <Col xs='3'>
-      <Input type='text' autoCapitalize='none' name={'w' + (4 * num + 1)} placeholder={(4 * num + 1)} />
+      <Input type='text' autoCapitalize='none' name={'w' + (4 * num + 1)} placeholder={4 * num + 2} />
     </Col>
     <Col xs='3'>
-      <Input type='text' autoCapitalize='none' name={'w' + (4 * num + 2)} placeholder={(4 * num + 2)} />
+      <Input type='text' autoCapitalize='none' name={'w' + (4 * num + 2)} placeholder={4 * num + 3} />
     </Col>
     <Col xs='3'>
-      <Input type='text' autoCapitalize='none' name={'w' + (4 * num + 3)} placeholder={(4 * num + 3)} />
+      <Input type='text' autoCapitalize='none' name={'w' + (4 * num + 3)} placeholder={4 * num + 4} />
     </Col>
   </FormGroup>
 )
@@ -32,11 +32,13 @@ const InputMnemonic = ({onBack, onWords}) => (
     <Row num={0} />
     <Row num={1} />
     <Row num={2} />
-    <Button color='primary' type='button' onClick={onBack}>
+    <Button color='danger' type='button' onClick={onBack}>
+      <span className='fa fa-arrow-left' style={{'margin-right': '5px'}} />
       Back
     </Button>
     <Button color='primary' type='submit' className='pull-right'>
       Confirm
+      <span className='fa fa-arrow-right' style={{'margin-left': '5px'}}/>
     </Button>
   </Form>
 )
