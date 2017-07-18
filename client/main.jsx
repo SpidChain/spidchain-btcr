a10a43ea74958c15fc89598b690bf105fe621d3e
Nicola Squartini <tensor5@gmail.com>
2017-07-18 17:32:20 +0200

    Initial bcoin integration

--- client/main.jsx
+++ client/main.jsx
@@ -1,9 +1,104 @@
+/* global bcoin */
+
 import {Meteor} from 'meteor/meteor'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from '/imports/ui/App'
 
+const network = Meteor.settings.public.network === 'testnet'
+  ? 'testnet'
+  : 'main'
+
 Meteor.startup(() => {
+//  bcoin.set(network)
+  bcoin.workerpool.enable()
+  const node = new bcoin.fullnode({
+    prune: true,
+//    network,
+    db: 'memory',
+    port: 8080
+  })
+
+  const wdb = node.use(bcoin.walletplugin)
+
+  node.pool.on('peer', (peer) => {
+    console.log('Peer:', peer)
+  })
+
+  node.mempool.on('block', (block) => {
+    console.log('Block:', block)
+  })
+
+  node.mempool.on('tx', (tx) => {
+    console.log('Tx:', tx)
+  })
+
+  node.on('error', (err) => {
+    console.log('Err:', err)
+  })
+
+  node.open()
+    .then(() => node.connect())
+    .then(() => {
+      node.on('connect', (entry, block) => {
+        console.log('%s (%d) added to chain.', entry.rhash(), entry.height)
+      })
+
+      node.startSync()
+
+      wdb.primary.on('balance', () => {
+        console.log('Balance:', wdb.primary)
+      })
+    })
+    .catch((err) => {
+      throw err
+    })
+
+//  const chain = new bcoin.chain({
+//    db: 'memory',
+//    spv: true
+//  })
+
+//  const pool = new bcoin.pool({
+//    chain: chain,
+//    spv: true,
+//    maxPeers: 8
+//  })
+
+//  const walletdb = new bcoin.walletdb({ db: 'memory' })
+/*
+  pool.open().then(() => {
+    return walletdb.open()
+  }).then(() => {
+    return walletdb.create()
+  }).then((wallet) => {
+    console.log('Created wallet with address %s', wallet.getAddress('base58'))
+
+    // Add our address to the spv filter.
+    pool.watchAddress(wallet.getAddress())
+
+    // Connect, start retrieving and relaying txs
+    pool.connect().then(() => {
+      console.log('connected to pool');
+      // Start the blockchain sync.
+      pool.startSync()
+
+      pool.on('peer', (peer) => {
+        console.log('Peer:', peer);
+      })
+
+      pool.on('tx', (tx) => {
+        console.log('Tx:', tx);
+        walletdb.addTX(tx)
+      })
+
+      wallet.on('balance', (balance) => {
+        console.log('Balance updated.')
+        console.log(bcoin.amount.btc(balance.unconfirmed))
+      })
+    })
+  })
+*/
   ReactDOM.render(<App />, document.getElementById('app'))
 })
--- server/main.js
+++ server/main.js
@@ -1,5 +1,28 @@
+import http from 'http'
+import {Meteor} from 'meteor/meteor'
+//import socketIO from 'socket.io'
+
 import '/imports/methods/bitcoin'
 import '/imports/methods/blockchaininfo'
 import '/imports/methods/blockexplorer'
 import '/imports/methods/ipfs'
 import '/imports/methods/Messaging'
+import '/imports/methods/resolveDDO'
+import WSProxy from '/imports/wsproxy'
+
+Meteor.startup(() => {
+  const server = http.createServer()
+//  console.log(server);
+//  const io = socketIO(server)
+
+  const proxy = new WSProxy({
+    ports: [8333, 18333, 18444, 28333, 28901]
+  })
+
+  proxy.on('error', (err) => {
+    console.error(err.stack + '')
+  })
+
+  proxy.attach(server)
+  server.listen(8333)
+})
