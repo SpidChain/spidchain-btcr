4e80ef08e8f820a5f4806ed84665312606b6d515
Federico Squartini <federico.squartini@gmail.com>
2017-09-05 12:23:28 +0200

    All transaction functions can also use sochain API

--- client/utils/txUtils.js
+++ client/utils/txUtils.js
@@ -1,18 +1,51 @@
-//import {Meteor} from 'meteor/meteor'
-
+import _ from 'lodash'
 import axios from 'axios'
-import bitcoinRpc from 'bitcoin/bitcoinRpc'
 
 const corsProxyUrl = process.env.corsProxyUrl
 
 const blockcypherApi = process.env.network === 'testnet'
-    ? corsProxyUrl + 'https://api.blockcypher.com/v1/btc/test3'
-    : corsProxyUrl + 'https://api.blockcypher.com/v1/btc/main'
+    ? corsProxyUrl + 'https://api.blockcypher.com/v1/btc/test3/'
+    : corsProxyUrl + 'https://api.blockcypher.com/v1/btc/main/'
+
+const soChainApi = process.env.network === 'testnet'
+   ? (method) => `https://chain.so/api/v2/${method}/BTCTEST/`
+   : (method) => `https://chain.so/api/v2/${method}/BTC/`
 
 export const getSpendingTx = async (txId, ix) => {
-  const {data} = await axios.get(blockcypherApi + `/txs/${txId}`)
-  return data.outputs[ix].spent_by || null
+  // block cypher
+  let blockcypherRes
+  try {
+    const {status: blockcypherStatus, data: cyData} =
+      await axios.get(blockcypherApi + `/txs/${txId}`)
+    blockcypherRes = blockcypherStatus !== 200 || cyData.error
+      ? 'failed' : cyData.outputs[ix].spent_by
+      ? cyData.outputs[ix].spent_by
+      : 'unspent'
+  } catch (e) {
+    blockcypherRes = {failed: true, message: e.message}
+  }
+  // sochain
+  let sochainRes
+  const sochainUrl = soChainApi('is_tx_spent') + txId + '/' + ix
+  try {
+    const {status: soStatus, data: soData} = await axios.get(sochainUrl)
+    sochainRes = soStatus !== 200 || soData.status === 'fail'
+      ? 'failed' : soData.data.is_spent
+      ? soData.data.spent.txid
+      : 'unspent'
+    //  soData.spent.input_no
+  } catch (e) {
+    sochainRes = {failed: true, message: e.message}
+  }
+  const result = _.filter([sochainRes, blockcypherRes],
+    e => !e.failed && e !== 'failed')
+  const firstTxId = _.head(result)
+  return firstTxId && _.every(result, e => e === firstTxId)
+    ? firstTxId
+    : null
 }
+
+window.getSpendingTx = getSpendingTx
 
 export const followFirstOut = async (txId) => {
   let tip
@@ -25,21 +58,59 @@ export const followFirstOut = async (txId) => {
 }
 
 export const getTxInfo = async (txId) => {
-  const {data: {block_hash, block_height, block_index, hex}} = await
-    axios.get(blockcypherApi + `/txs/${txId}`, {
-      params: {
-        includeHex: true
+  let blockcypherRes
+  try {
+    const {
+      status: blockcypherStatus,
+      data: {error, block_hash, block_height, block_index, confirmed, hex}} =
+      await axios.get(blockcypherApi + `/txs/${txId}`, {
+        params: {
+          includeHex: true
+        }
+      })
+    blockcypherRes = blockcypherStatus !== 200 || error
+      ? 'failed'
+      : {
+        blockHash: block_hash,
+        blockTime: new Date(confirmed),
+        height: block_height,
+        ix: block_index,
+        tx: hex
       }
-    })
-  const block = await bitcoinRpc('getBlockHeader', block_hash)
-  return {
-    blockHash: block_hash,
-    blockTime: block.time,
-    height: block_height,
-    ix: block_index,
-    tx: hex
+  } catch (e) {
+    blockcypherRes = {failed: true, message: e.message}
   }
+  let sochainRes
+  const sochainUrl = soChainApi('get_tx') + txId
+  try {
+    const {status: soStatus, data: soData} = await axios.get(sochainUrl)
+    sochainRes = soStatus !== 200 || soData.status !== 'success'
+      ? 'failed'
+      : { tx: soData.data.tx_hex,
+        blockTime: new Date(soData.data.time * 1000),
+        blockHash: soData.data.blockhash }
+    if (sochainRes !== 'failed') {
+      const sochainUrl2 = soChainApi('get_block') + sochainRes.blockHash
+      const {status: soStatus2, data: soData2} = await axios.get(sochainUrl2)
+      sochainRes = soStatus2 !== 200 || soData2.status !== 'success'
+        ? 'failed'
+        : { ...sochainRes,
+          ix: _.findIndex(soData2.data.txs, t => t === txId),
+          height: soData2.data.block_no
+        }
+    }
+  } catch (e) {
+    sochainRes = {failed: true, message: e.message}
+  }
+  const result = _.filter([sochainRes, blockcypherRes],
+    e => !e.failed && e !== 'failed')
+  const firstRes = _.head(result)
+  return firstRes && _.every(result, e => _.isEqual(e, firstRes))
+    ? firstRes
+    : null
 }
+
+window.getTxInfo = getTxInfo
 
 export const getPath = async (txId) => {
   const path = []
@@ -53,6 +124,32 @@ export const getPath = async (txId) => {
 }
 
 export const txrefToTxid = async (height, index) => {
-  const {data: {txids}} = await axios.get(blockcypherApi + `/blocks/${height}?txstart=${index}&limit=1`)
-  return txids[0]
+  let blockcypherRes
+  try {
+    const {status: blockcypherStatus, data: {error, txids}} =
+      await axios.get(blockcypherApi + `/blocks/${height}?txstart=${index}&limit=1`)
+    blockcypherRes = blockcypherStatus !== 200 || error
+      ? 'failed'
+      : txids[0]
+  } catch (e) {
+    blockcypherRes = {failed: true, message: e.message}
+  }
+  let sochainRes
+  try {
+    const sochainUrl = soChainApi('get_block') + height
+    const {status: soStatus, data: soData} = await axios.get(sochainUrl)
+    sochainRes = soStatus !== 200 || soData.status !== 'success'
+      ? 'failed'
+      : soData.data.txs[index]
+  } catch (e) {
+    sochainRes = {failed: true, message: e.message}
+  }
+  const result = _.filter([sochainRes, blockcypherRes],
+    e => !e.failed && e !== 'failed')
+  const firstRes = _.head(result)
+  return firstRes && _.every(result, res => res === firstRes)
+    ? firstRes
+    : null
 }
+
+window.txrefToTxid = txrefToTxid
