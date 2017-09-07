6d6546e14e20435c44161edabb2e352309ea2671
Federico Squartini <federico.squartini@gmail.com>
2017-09-08 00:13:36 +0200

    Remove old files

--- client/bitcoin/sendRawTransaction.js
+++
@@ -1,7 +0,0 @@
-//import {Meteor} from 'meteor/meteor'
-
-//import '/imports/methods/bitcoin'
-import 'bitcoin/bitcoinRpc'
-
-//export default (rawTx) => Meteor.callPromise('bitcoin', 'sendRawTransaction', rawTx)
-export default (rawTx) => bitcoinRpc('sendRawTransaction', rawTx)
