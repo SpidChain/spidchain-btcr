be5bdff7dd112a88ceae8eb7dfbff99ed838c2d0
Federico Squartini <federico.squartini@gmail.com>
2017-09-08 16:54:14 +0200

    Make App a functional component

--- client/ui/App.jsx
+++ client/ui/App.jsx
@@ -1,10 +1,9 @@
-import createReactClass from 'create-react-class'
 import React from 'react'
 import {connect} from 'react-redux'
 import {BrowserRouter, Route} from 'react-router-dom'
 import 'react-notifications/dist/react-notifications.css'
 import {NotificationContainer} from 'react-notifications'
-import {Col, Container, Row} from 'reactstrap'
+import {Container} from 'reactstrap'
 import Spinner from 'react-spinkit'
 
 import ActivationFlow from 'ui/ActivationFlow'
@@ -25,38 +24,33 @@ const centerElement = {
   justifyContent: 'center'
 }
 
-const App = createReactClass({
-  displayName: 'App',
-
-  render () {
-    const {did, wallet} = this.props
-    if (did.loading || wallet.loading) {
-      return (
-        <Container fluid style={centerElement}>
-          <Spinner name='double-bounce' />
-        </Container>
-      )
-    }
-    return wallet && wallet && wallet.root && did && did.did
-      ? <BrowserRouter>
-        <div>
-          <NavBar />
-          <Route exact path='/' component={Home} />
-          <Route exact path='/addContact' component={AddContact} />
-          <Route exact path='/contactRequests' component={ContactRequests} />
-          <Route exact path='/contacts' component={Contacts} />
-          <Route exact path='/generateClaim' component={GenerateClaim} />
-          <Route exact path='/developer' component={Developer} />
-          <Route exact path='/ownClaims' component={OwnClaims} />
-          <Route exact path='/othersClaims' component={OthersClaims} />
-          <NotificationContainer />
-        </div>
-      </BrowserRouter>
-      : <div>
-        <ActivationFlow />
+const App = ({did, wallet}) => {
+  if (did.loading || wallet.loading) {
+    return (
+      <Container fluid style={centerElement}>
+        <Spinner name='double-bounce' />
+      </Container>
+    )
+  }
+  return wallet && wallet && wallet.root && did && did.did
+    ? <BrowserRouter>
+      <div>
+        <NavBar />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/addContact' component={AddContact} />
+        <Route exact path='/contactRequests' component={ContactRequests} />
+        <Route exact path='/contacts' component={Contacts} />
+        <Route exact path='/generateClaim' component={GenerateClaim} />
+        <Route exact path='/developer' component={Developer} />
+        <Route exact path='/ownClaims' component={OwnClaims} />
+        <Route exact path='/othersClaims' component={OthersClaims} />
         <NotificationContainer />
       </div>
-  }
-})
+    </BrowserRouter>
+    : <div>
+      <ActivationFlow />
+      <NotificationContainer />
+    </div>
+}
 
 export default connect(s => s)(App)
