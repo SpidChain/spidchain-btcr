3f829d32bbc1ba21dd1da28fcbb2db930b783804
Federico Squartini <federico.squartini@gmail.com>
2017-09-08 13:37:17 +0200

    Improve error handling, fix bug in getSpendingTx

--- client/utils/txUtils.js
+++ client/utils/txUtils.js
@@ -19,9 +19,9 @@ export const getSpendingTx = async (txId, ix) => {
     const {status: blockcypherStatus, data: cyData} =
       await axios.get(blockcypherApi + `/txs/${txId}`)
     blockcypherRes = blockcypherStatus !== 200 || cyData.error
-      ? 'failed' : cyData.outputs[ix].spent_by
+      ? {failed: true} : cyData.outputs[ix].spent_by
       ? cyData.outputs[ix].spent_by
-      : 'unspent'
+      : null
   } catch (e) {
     blockcypherRes = {failed: true, message: e.message}
   }
@@ -31,19 +31,22 @@ export const getSpendingTx = async (txId, ix) => {
   try {
     const {status: soStatus, data: soData} = await axios.get(sochainUrl)
     sochainRes = soStatus !== 200 || soData.status === 'fail'
-      ? 'failed' : soData.data.is_spent
+      ? {failed: true} : soData.data.is_spent
       ? soData.data.spent.txid
-      : 'unspent'
+      : null
     //  soData.spent.input_no
   } catch (e) {
     sochainRes = {failed: true, message: e.message}
   }
-  const result = _.filter([sochainRes, blockcypherRes],
-    e => !e.failed && e !== 'failed')
+  const result = _.filter([sochainRes, blockcypherRes], e => e === null || !e.failed)
+  if (_.isEmpty(result)) {
+    throw new Error('No results from block-explorers')
+  }
   const firstTxId = _.head(result)
-  return firstTxId && _.every(result, e => e === firstTxId)
-    ? firstTxId
-    : null
+  if (!_.every(result, e => e === firstTxId)) {
+    throw new Error('Block-explorers results mismatch')
+  }
+  return firstTxId
 }
 
 window.getSpendingTx = getSpendingTx
@@ -63,14 +66,14 @@ export const getTxInfo = async (txId) => {
   try {
     const {
       status: blockcypherStatus,
-      data: {error, block_hash, block_height, block_index, confirmed, hex}} =
-      await axios.get(blockcypherApi + `/txs/${txId}`, {
-        params: {
-          includeHex: true
-        }
-      })
+      data: {error, block_hash, block_height, block_index, confirmed, hex}
+    } = await axios.get(blockcypherApi + `/txs/${txId}`, {
+      params: {
+        includeHex: true
+      }
+    })
     blockcypherRes = blockcypherStatus !== 200 || error
-      ? 'failed'
+      ? {failed: true}
       : {
         blockHash: block_hash,
         blockTime: new Date(confirmed),
@@ -86,15 +89,15 @@ export const getTxInfo = async (txId) => {
   try {
     const {status: soStatus, data: soData} = await axios.get(sochainUrl)
     sochainRes = soStatus !== 200 || soData.status !== 'success'
-      ? 'failed'
+      ? {failed: true}
       : { tx: soData.data.tx_hex,
         blockTime: new Date(soData.data.time * 1000),
         blockHash: soData.data.blockhash }
-    if (sochainRes !== 'failed') {
+    if (!sochainRes.failed) {
       const sochainUrl2 = soChainApi('get_block') + sochainRes.blockHash
       const {status: soStatus2, data: soData2} = await axios.get(sochainUrl2)
       sochainRes = soStatus2 !== 200 || soData2.status !== 'success'
-        ? 'failed'
+        ? {failed: true}
         : { ...sochainRes,
           ix: _.findIndex(soData2.data.txs, t => t === txId),
           height: soData2.data.block_no
@@ -103,12 +106,15 @@ export const getTxInfo = async (txId) => {
   } catch (e) {
     sochainRes = {failed: true, message: e.message}
   }
-  const result = _.filter([sochainRes, blockcypherRes],
-    e => !e.failed && e !== 'failed')
+  const result = _.filter([sochainRes, blockcypherRes], e => !e.failed)
+  if (_.isEmpty(result)) {
+    throw new Error('No results from the explorers')
+  }
   const firstRes = _.head(result)
-  return firstRes && _.every(result, e => _.isEqual(e, firstRes))
-    ? firstRes
-    : null
+  if (!_.every(result, e => _.isEqual(e, firstRes))) {
+    throw new Error('Block-explorers results mismatch')
+  }
+  return firstRes
 }
 
 export const getPath = async (txId) => {
@@ -128,7 +134,7 @@ export const txrefToTxid = async (height, index) => {
     const {status: blockcypherStatus, data: {error, txids}} =
       await axios.get(blockcypherApi + `/blocks/${height}?txstart=${index}&limit=1`)
     blockcypherRes = blockcypherStatus !== 200 || error
-      ? 'failed'
+      ? {failed: true}
       : txids[0]
   } catch (e) {
     blockcypherRes = {failed: true, message: e.message}
@@ -138,17 +144,20 @@ export const txrefToTxid = async (height, index) => {
     const sochainUrl = soChainApi('get_block') + height
     const {status: soStatus, data: soData} = await axios.get(sochainUrl)
     sochainRes = soStatus !== 200 || soData.status !== 'success'
-      ? 'failed'
+      ? {failed: true}
       : soData.data.txs[index]
   } catch (e) {
     sochainRes = {failed: true, message: e.message}
   }
-  const result = _.filter([sochainRes, blockcypherRes],
-    e => !e.failed && e !== 'failed')
+  const result = _.filter([sochainRes, blockcypherRes], e => !e.failed)
+  if (_.isEmpty(result)) {
+    throw new Error('No results from the explorers')
+  }
   const firstRes = _.head(result)
-  return firstRes && _.every(result, res => res === firstRes)
-    ? firstRes
-    : null
+  if (!_.every(result, e => e === firstRes)) {
+    throw new Error('Block-explorers results mismatch')
+  }
+  return firstRes
 }
 
 export const listUtxos = async (address) => {
@@ -158,7 +167,7 @@ export const listUtxos = async (address) => {
     const {status: blockcypherStatus, data: {error, txrefs}} =
       await axios.get(url, {params: {unspentOnly: true}})
     blockcypherRes = blockcypherStatus !== 200 || error
-      ? 'failed'
+      ? {failed: true}
       : txrefs.map(({value, tx_output_n, tx_hash}) =>
         ({value, txid: tx_hash, vout: tx_output_n}))
   } catch (e) {
@@ -169,7 +178,7 @@ export const listUtxos = async (address) => {
     const sochainUrl = soChainApi('get_tx_unspent') + address
     const {status: soStatus, data: soData} = await axios.get(sochainUrl)
     sochainRes = soStatus !== 200 || soData.status !== 'success'
-      ? 'failed'
+      ? {failed: true}
       : soData.data.txs
       .map(({txid, output_no, value}) =>
         ({value: sb.toSatoshi(value), txid, vout: output_no}))
@@ -177,12 +186,16 @@ export const listUtxos = async (address) => {
     sochainRes = {failed: true, message: e.message}
   }
   const result = [sochainRes, blockcypherRes]
-    .filter(e => !e.failed && e !== 'failed')
+    .filter(e => !e.failed)
     .map(e => _.sortBy(e, ['txid']))
+  if (_.isEmpty(result)) {
+    throw new Error('No results from the explorers')
+  }
   const firstRes = _.head(result)
-  return firstRes && _.every(result, res => _.isEqual(res, firstRes))
-    ? firstRes
-    : null
+  if (!_.every(result, e => _.isEqual(e, firstRes))) {
+    throw new Error('Block-explorers results mismatch')
+  }
+  return firstRes
 }
 
 export const getBalance = async (address) => {
@@ -192,7 +205,7 @@ export const getBalance = async (address) => {
     const {status: blockcypherStatus, data: {error, balance}} =
       await axios.get(url)
     blockcypherRes = blockcypherStatus !== 200 || error
-      ? 'failed'
+      ? {failed: true}
       : balance
   } catch (e) {
     blockcypherRes = {failed: true, message: e.message}
@@ -202,17 +215,21 @@ export const getBalance = async (address) => {
     const sochainUrl = soChainApi('get_address_balance') + address + '/0'
     const {status: soStatus, data: soData} = await axios.get(sochainUrl)
     sochainRes = soStatus !== 200 || soData.status !== 'success'
-      ? 'failed'
+      ? {failed: true}
       : sb.toSatoshi(soData.data.confirmed_balance)
   } catch (e) {
     sochainRes = {failed: true, message: e.message}
   }
   const result = _.filter([sochainRes, blockcypherRes],
-    e => !e.failed && e !== 'failed')
+    e => !e.failed && _.isNumber(e))
+  if (_.isEmpty(result)) {
+    throw new Error('No results from the explorers')
+  }
   const firstRes = _.head(result)
-  return _.isNumber(firstRes) && _.every(result, res => res === firstRes)
-    ? firstRes
-    : null
+  if (!_.every(result, e => e === firstRes)) {
+    throw new Error('Block-explorers results mismatch')
+  }
+  return firstRes
 }
 
 /*
