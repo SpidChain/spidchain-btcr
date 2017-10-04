0dda71a1d96729abea9cdeef07f25f7f96c7b28c
Federico Squartini <federico.squartini@gmail.com>
2017-10-04 16:46:02 +0200

    Change resposive transition point in the home screen

--- client/ui/Home.jsx
+++ client/ui/Home.jsx
@@ -9,19 +9,19 @@ import Balance from 'ui/Balance'
 const Home = ({did, wallet}) => {
   return <Container fluid>
     <Row className='mt-3'>
-      <Col md='6' className='mx-auto'>
+      <Col sm='6' className='mx-auto'>
         <Icon className='w-75 d-block mx-auto mt-3' alt='SpidChain icon' />
       </Col>
     </Row>
     <Row className='mt-3'>
-      <Col md='6' className='mx-auto'>
+      <Col sm='6' className='mx-auto'>
         <ShowQRCode content={wallet.receivingAddress}>
           <Balance />
         </ShowQRCode>
       </Col>
     </Row>
     <Row className='mt-3'>
-      <Col md='6' className='mx-auto'>
+      <Col sm='6' className='mx-auto'>
         <ShowQRCode content={did.did} >
           Show DID
         </ShowQRCode>
